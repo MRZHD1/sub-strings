b9a5945fa70d8229c913b6f893400b8091c95282
Omer Ali <75752223+MRZHD1@users.noreply.github.com>
2022-10-08 21:29:10 +0300

    Adding expected return

--- main.rb
+++ main.rb
@@ -6,4 +6,5 @@ def substring(string, array)
   result.select { |_, value| value.positive? }
 end
 
-substring("Howdy partner, sit down! How's it going?", dictionary)
+substring("Howdy partner, sit down! How's it going?", dictionary) 
+# returns => { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
