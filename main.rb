9dcb5624a7a9696f698aaaf34b5259cc7a968156
Omer <omerzahidali@gmail.com>
2022-10-08 20:04:33 +0300

    style changes

--- main.rb
+++ main.rb
@@ -1,11 +1,9 @@
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
 
 def substring(string, array)
-    result = Hash.new(0)
-    for sub in array
-        result[sub] = string.downcase.scan(/#{sub}/).length
-    end
-    return result.select{|key, value| value > 0 }
+  result = Hash.new(0)
+  array.each { |sub| result[sub] = string.downcase.scan(/#{sub}/).length }
+  result.select { |_, value| value.positive? }
 end
 
 substring("Howdy partner, sit down! How's it going?", dictionary)
